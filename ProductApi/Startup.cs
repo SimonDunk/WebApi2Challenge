3f74dfa040d9879a8182dc3385c07141c9afefc1
SimonDunk <simon.j.dunkley@gmail.com>
2018-07-23 18:50:29 +1000

    Refactored to use the correct project type for authentication

--- ProductApi/Startup.cs
+++ ProductApi/Startup.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
 using ProductApi.Models;
 
 namespace ProductApi
@@ -21,14 +22,33 @@ namespace ProductApi
         private string[] descs = { "large tv", "22in monitor", "4k tv", "1080p tv", "144p tv" };
         private string[] models = { "A100", "IPS9000", "OLED117" };
         private string[] brands = { "GL", "SUSA", "ACE", "PAN", "TKL" };
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             //database context is registered with dependency injection (DI) container
             //services that are registered with DI are available to the controllers
             //specifies an in memory database and is injected into the service container
             services.AddDbContext<ProductContext>(opt => opt.UseInMemoryDatabase("ProductList"));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -36,10 +56,34 @@ namespace ProductApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            //app.UseDefaultFiles();
             var context = serviceProvider.GetService<ProductContext>();
             PopulateDB(context, 20);
 
-            app.UseMvc();
+            app.UseAuthentication();
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
 
         //filling database with products
---
+++ ProductApi/wwwroot/js/site.js
@@ -0,0 +1,147 @@
+﻿const uri = 'api/product';
+let products = null;
+
+//counts all elements in database
+function getCount(data) {
+    const el = $('#counter');
+    let name = 'product';
+    if (data) {
+        if (data > 1) {
+            name = 'products';
+        }
+        el.text(data + ' ' + name);
+    } else {
+        el.html('No ' + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+//gets a list of to do items sending an HTTP GET request to /api/product
+function getData(txt) {
+    $.ajax({
+        type: 'GET', //request type
+        url: uri, // request url
+        success: function (data) { //callback function when request succeeds
+            $('#products').empty();
+            getCount(data.length);
+            $.each(data, function (key, item) {
+                $('<tr><td>' + item.id + '</td>' +
+                    '<td>' + '<a onclick="getSpecificData(\'' + item.description + '\')">' + item.description + '</a></td>' +
+                    '<td>' + '<a onclick="getSpecificData(\'' + item.model + '\')">' + item.model + '</a></td>' + '</td>' +
+                    '<td>' + '<a onclick="getSpecificData(\'' + item.brand + '\')">' + item.brand + '</a></td>' + '</td>' +
+                    '<td><button onclick="editItem(\'' + item.id + '\')">Edit</button></td>' +
+                    '<td><button onclick="deleteItem(\'' + item.id + '\');">Delete</button></td>' +
+                    '</tr>').appendTo($('#products'));
+            });
+            //DOM is updated with product information
+            products = data;
+        }
+    });
+}
+
+//adds a product sending a HTTP POST request to /api/product
+function addItem() {
+    //contains a product
+    const item = {
+        'description': $('#add-description').val(),
+        'model': $('#add-model').val(),
+        'brand': $('#add-brand').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        accepts: 'application/json',
+        url: uri,
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('here');
+        },
+        success: function (result) { //when successful refresh HTML table and remove inputs
+            getData();
+            $('#add-description').val('');
+            $('#add-model').val('');
+            $('#add-brand').val('');
+        }
+    });
+}
+
+//deleting a product by id sending a HTTP DELETE request to /api/product
+function deleteItem(id) {
+    $.ajax({
+        url: uri + '/' + id,
+        type: 'DELETE',
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+//shows input fields if id is correct
+function editItem(id) {
+    $.each(products, function (key, item) {
+        if (item.id === id) {
+            $('#edit-id').val(item.id);
+            $('#edit-description').val(item.description);
+            $('#edit-model').val(item.model);
+            $('#edit-brand').val(item.brand);
+        }
+    });
+    $('#spoiler').css({ 'display': 'block' });
+}
+
+//updating a product by id sending a HTTP PUT request to /api/product
+$('.my-form').on('submit', function () {
+    const item = {
+        'id': $('#edit-id').val(),
+        'description': $('#edit-description').val(),
+        'model': $('#edit-model').val(),
+        'brand': $('#edit-brand').val()
+    };
+
+    $.ajax({
+        url: uri + '/' + $('#edit-id').val(),
+        type: 'PUT',
+        accepts: 'application/json',
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+//closes edit input
+function closeInput() {
+    $('#spoiler').css({ 'display': 'none' });
+}
+
+function getSpecificData(txt) {
+    $.ajax({
+        type: 'GET', //request type
+        url: uri, // request url
+        success: function (data) { //callback function when request succeeds
+            $('#products').empty();
+            getCount(data.length);
+            $.each(data, function (key, item) {
+                if (item.description === txt || item.model === txt || item.brand === txt) {
+                    $('<tr><td>' + item.id + '</td>' +
+                        '<td>' + '<a onclick="getSpecificData(\'' + item.description + '\')">' + item.description + '</a></td>' +
+                        '<td>' + '<a onclick="getSpecificData(\'' + item.model + '\')">' + item.model + '</a></td>' + '</td>' +
+                        '<td>' + '<a onclick="getSpecificData(\'' + item.brand + '\')">' + item.brand + '</a></td>' + '</td>' +
+                        '<td><button onclick="editItem(\'' + item.id + '\')">Edit</button></td>' +
+                        '<td><button onclick="deleteItem(\'' + item.id + '\');">Delete</button></td>' +
+                        '</tr>').appendTo($('#products'));
+                }             
+            });
+            //DOM is updated with product information
+            products = data;
+        }
+    });
+}
